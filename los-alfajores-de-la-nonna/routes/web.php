e3f07870d55c4459ee522ae4af41aa6114901471
sebitacasa <seba.-14@hotmail.com>
2026-06-02 12:17:39 +0200

    Estructura inicial, catálogo y formulario de encargos

--- los-alfajores-de-la-nonna/routes/web.php
+++ los-alfajores-de-la-nonna/routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+// Cambiamos Mail por Log temporalmente
+use Illuminate\Support\Facades\Log; 
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/encargar', function () {
+    return view('encargar');
+});
+
+Route::post('/encargar', function (Request $request) {
+    // 1. Validamos los datos
+    $datos = $request->validate([
+        'nombre' => 'required',
+        'email' => 'required|email',
+        'pedido' => 'required',
+        'detalles' => 'nullable'
+    ]);
+
+    // 2. EN LUGAR DE ENVIAR EMAIL, LO GUARDAMOS EN EL REGISTRO DE LARAVEL
+    Log::info('NUEVO PEDIDO RECIBIDO:', $datos);
+
+    // 3. Devolvemos al cliente a la portada con un mensaje de éxito
+    return redirect('/')->with('exito', '¡Tu encargo ha sido enviado con éxito! Te contactaremos pronto.');
 });
